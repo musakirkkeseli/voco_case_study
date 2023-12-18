31727adfaacd874bcb00f5195503eb50c77ee778
Musa KIRKKESELİ <musakirkkeseli@gmail.com>
2023-12-18 14:06:25 +0300

    Processes to run when the application starts

---
+++ lib/core/app_initialize.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:voco_case_study/core/cache_manager.dart';
+import 'package:voco_case_study/core/widget/app_error_widget.dart';
+
+final class AppInitialze {
+  AppInitialze._();
+  static Future<void> initialize() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    //Uygulama CacheManager sınıfı çalıştırır
+    await CacheManager.db.init();
+    //uygulamada yaşanacak beklenmedik büyük hatalarda ekrana AppErrorWidget widgeti gösterilir
+    ErrorWidget.builder = (FlutterErrorDetails details) {
+      return const AppErrorWidget();
+    };
+  }
+}
