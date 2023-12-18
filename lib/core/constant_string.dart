facec5863a5bf08f3786b0272cc79a23064c1e29
Musa KIRKKESELİ <musakirkkeseli@gmail.com>
2023-12-18 14:12:47 +0300

    A class was created to hold application-wide string expressions.

---
+++ lib/core/constant_string.dart
@@ -0,0 +1,14 @@
+class ConstantsString {
+  ConstantsString._();
+
+  static const String baseUrl = "https://reqres.in/";
+  static const String homeAppbarTitle = "Kullanıcı Listesi";
+  static const String login = 'Login';
+  static const String register = 'Register';
+  static const String gotoRegister = 'Create an account';
+  static const String gotoLogin = 'Back to Login';
+  static const String textFieldEmptyWarning = 'This field is required';
+  static const String minCharacterWarning = 'Please at least 8 characters';
+  static const String vocoIconUrl =
+      'https://play-lh.googleusercontent.com/pNGHxHX2B7h-WqNViSPY-Y9pLodmqQaEgenK_3Ne6e7jbLayvxEgX2toWXvS7g1T-EQ=w240-h480-rw';
+}
