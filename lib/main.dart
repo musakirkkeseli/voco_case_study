477b9dddf9d31500bd79b268844dddd0af34f2ae
Musa KIRKKESELİ <musakirkkeseli@gmail.com>
2023-12-18 14:18:45 +0300

    All processes are merged on the main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:voco_case_study/core/app_initialize.dart';
+import 'package:voco_case_study/core/cache_manager.dart';
+import 'package:voco_case_study/product/auth/view/auth_view.dart';
+import 'package:voco_case_study/product/home/view/home_view.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  await AppInitialze.initialize();
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -10,7 +16,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Voco Case Study',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -30,6 +37,8 @@ class MainWidget extends StatefulWidget {
 class _MainWidgetState extends State<MainWidget> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return CacheManager.db.getToken() == ""
+        ? const AuthView()
+        : const HomeView();
   }
 }
