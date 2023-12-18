fd42ef7c26fee4a79cccaf90a38f5b25e8d0ae8d
Musa KIRKKESELİ <musakirkkeseli@gmail.com>
2023-12-18 14:15:00 +0300

    home_view page created

---
+++ lib/product/home/view/home_view.dart
@@ -0,0 +1,77 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:voco_case_study/core/constant_string.dart';
+import 'package:voco_case_study/core/widget/loading_widget.dart';
+import 'package:voco_case_study/product/auth/view/auth_view.dart';
+import 'package:voco_case_study/product/home/model/list_user_response.dart';
+import 'package:voco_case_study/product/home/riverpod/home_riverpod.dart';
+import 'package:voco_case_study/product/home/service/home_service.dart';
+
+part 'widget/user_listview.dart';
+
+final homeRiverpod = ChangeNotifierProvider((ref) => HomeRiverpod(
+    HomeService(Dio(BaseOptions(baseUrl: ConstantsString.baseUrl)))));
+
+class HomeView extends ConsumerStatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<HomeView> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    ref.read(homeRiverpod).firstFetch();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var readHome = ref.read(homeRiverpod);
+    var watchHome = ref.watch(homeRiverpod);
+    ref.listen(homeRiverpod, (previous, next) {
+      switch (next.status) {
+        case HomeStatus.logout:
+          Navigator.pushReplacement(context,
+              MaterialPageRoute(builder: (context) => const AuthView()));
+          break;
+        default:
+      }
+    });
+    ListUsersResponseModel listUsersResponseModel =
+        watchHome.listUsersResponseModel ?? ListUsersResponseModel();
+    List<Data> userList = (listUsersResponseModel.data ?? []);
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(ConstantsString.homeAppbarTitle),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  readHome.logout();
+                },
+                icon: const Icon(Icons.logout))
+          ],
+        ),
+        body: body(watchHome, userList));
+  }
+
+  body(HomeRiverpod watchHome, List<Data> userList) {
+    switch (watchHome.status) {
+      case HomeStatus.initial:
+        return const LoadingWidget();
+      case HomeStatus.success:
+        return UserListview(
+            scrollController: _scrollController,
+            watchHome: watchHome,
+            userList: userList);
+      default:
+        return const Center(
+          child: Text("Beklenmedik bir hata oluştu"),
+        );
+    }
+  }
+}
